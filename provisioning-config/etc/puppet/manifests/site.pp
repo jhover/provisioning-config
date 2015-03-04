704dfb6a3e61019a87a5843b43129a63349a06c3
/DC=com/DC=DigiCert-Grid/O=Open Science Grid/OU=People/CN=John Hover 241 </DC=com/DC=DigiCert-Grid/O=Open Science Grid/OU=People/CN=John Hover 241@3ae89d03-1715-0410-8ddc-c8e1f6537b43>
2015-03-04 15:21:13 +0000

    Update

--- provisioning-config/etc/puppet/manifests/site.pp
+++ provisioning-config/etc/puppet/manifests/site.pp
@@ -1,3 +1,12 @@
 node default {
   hiera_include('classes')
 }
+
+if versioncmp($::puppetversion,'3.6.1') >= 0 {
+
+  $allow_virtual_packages = hiera('allow_virtual_packages',false)
+
+  Package {
+    allow_virtual => $allow_virtual_packages,
+  }
+}
